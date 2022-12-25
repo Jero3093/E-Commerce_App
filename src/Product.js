35809c3fb3f5d7f615132cc7134621441303e83d
Jeronimo <jeronimotornini@gmail.com>
2022-12-25 18:03:42 -0300

    Creacion de JSON con lista de productos

---
+++ src/Product.js
@@ -0,0 +1,88 @@
+export const Products = [
+  {
+    id: 1,
+    name: "GEARONIC TM Mochila de lona vintage de 21 L para hombres Mochila de cuero Mochila de 15 pulgadas para portátil Mochila de hombro del ejército militar Mochila de hombro Mochila de senderismo Bolsa de café.",
+    description:
+      "Esta mochila de lona grande tiene un tamaño aproximado de 16 x 18 x 9 pulgadas de profundidad. Lo suficientemente grande como para adaptarse a los elementos esenciales del día a día, como laptop, cuaderno, libro, iPad, almuerzo, cámara, equipo de afeitado, prendas, ropa u otros artículos y accesorios personales esenciales que necesite. MATERIALES DE ALTA CALIDAD: nuestra bolsa de campo Gearonic TM está hecha con tela de lona duradera de alta densidad de alta calidad y cuero PU, esta resistente bolsa cuenta con un diseño clásico pero elegante con solapa de cierre de botón, detalles de hardware de latón vintage, correa para el hombro ajustable y cómoda y múltiples bolsillos para mantenga sus artículos esenciales diarios organizados.",
+    price: "1032",
+    uri: "https://m.media-amazon.com/images/I/612O7H6lmrL._AC_UL1000_.jpg",
+  },
+  {
+    id: 2,
+    name: "Chaqueta bomber de algodón COOFANDY, de corte ajustado, informal, para hombre.",
+    description:
+      "Chaqueta de algodón mullido con cierre de botones de poliéster que proporciona calidez en los días de otoño/principios de invierno: ya sea en una mañana helada o en una noche fresca, una chaqueta cómoda siempre es útil. Y una chaqueta universitaria de Coofandy es una opción increíble que funciona totalmente aquí. Chaqueta bomber de punto elegante del más alto estándar de calidad: detalles clásicos como manga larga sin color, cierre de botones, cuello, puños y dobladillo acanalados y bolsillos funcionales",
+    price: "2132",
+    uri: "https://m.media-amazon.com/images/I/71x60l-f+kL._AC_UL1500_.jpg",
+  },
+  {
+    id: 3,
+    name: "Mochila bandolera para hombres bandolera cruzada con USB impermeable ligero.",
+    description:
+      "Forro importado bien construido Cierre de cremallera. MATERIAL DURADERO Y RESISTENTE: El material NO es cuero. El bolso bandolera para hombre está hecho de material de PVC de alta densidad resistente al agua y al desgarro y en el interior de tela de poliéster. Las cremalleras metálicas abiertas de dos vías están diseñadas para una mayor durabilidad. Los accesorios de hebilla y anillo de la bolsa de pecho son de acero inoxidable, nunca se oxidan.",
+    price: "1232",
+    uri: "https://m.media-amazon.com/images/I/51e--EDkOGL._AC_UL1001_.jpg",
+  },
+  {
+    id: 4,
+    name: "Chaqueta de béisbol Moshtashio para hombre, estilo universitario, estilo vintage, informal, unisex, ropa de calle, con parche.",
+    description:
+      "Nuestra chaqueta universitaria está hecha de tela ligera y suave. Ligero y elegante, el mejor abrigo activo para la temporada de primavera, otoño e invierno para uso en interiores y exteriores. Chaqueta de corte completo con parche con la letra «B» en el pecho y bordado con patrón de huesos. Cuerpo de tela de alta calidad, mangas de piel sintética con puños elásticos. Bolsillos frontales inclinados. Cierre con botón a presión frontal.",
+    price: "3532",
+    uri: "https://m.media-amazon.com/images/I/613wanpF8BL._AC_UL1500_.jpg",
+  },
+  {
+    id: 5,
+    name: "Moshtashio Chaqueta universitaria universitaria para hombre, chaqueta bomber de béisbol, sudadera vintage, informal, unisex, ropa de calle, abrigos con parche.",
+    description:
+      "Cierre a presión de poliéster importado【TELA PREMIUM】Nuestra chaqueta universitaria está hecha de tela ligera y suave. Ligera y elegante, la mejor chaqueta activa para la temporada de primavera, otoño e invierno para uso en interiores y exteriores.【CARACTERÍSTICAS】Chaqueta de corte completo con un parche con la letra «B» en el pecho y bordado con patrón de huesos. Cuerpo de tela de alta calidad, mangas de piel sintética con puños elásticos. Bolsillos delanteros inclinados. Cierre frontal con botón a presión. Bajo acanalado.",
+    price: "5232",
+    uri: "https://m.media-amazon.com/images/I/71klDrXJ6eL._AC_UL1500_.jpg",
+  },
+  {
+    id: 6,
+    name: "Beaully Women's Flannel Plaid Jacket Long Sleeve Button Down Chest Pocketed Shirts Coats Shacket.",
+    description:
+      "[Material]-The button up shirt made with high quality flannel. The flannel fabric is soft, wear-resistant, and has good warmth retention.soft to the touch and comfortable to wear.best for autumn and winter. [Feature]-The flannel jacket,Button front top,plaid print,Scoop hemline,Two flap chest pocket,Single button on cuff,Soft fabrication,Oversized fit,Drop shoulder,Unlined.",
+    price: "2402",
+    uri: "https://m.media-amazon.com/images/I/61N9LO+XamL._AC_UL1001_.jpg",
+  },
+  {
+    id: 7,
+    name: "CHRLEISURE Butt Lifting Workout Leggings for Women, Scrunch Butt Gym Seamless Booty Tight.",
+    description:
+      "BUTT LIFTING LEGGING - This sensual push-up legging style will smooth both the legs and the tummy while targeted compression creates dramatic definition under the rear-end, Bum lifting bands design cinch your backside with invisible hold around your butt, Works like a push up bra for your booty, providing a little lift",
+    price: "670",
+    uri: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/81mFMa3z-wL._AC_UL1500_.jpg",
+  },
+  {
+    id: 8,
+    name: "Tableta Android Samsung Galaxy Tab A8, pantalla LCD de 10,5, almacenamiento de 32 GB, batería de larga duración, contenido para niños, interruptor inteligente, memoria ampliable, gris oscuro.",
+    description:
+      "Potencia y almacenamiento para todos: obtén la potencia, el almacenamiento y la velocidad que tu familia necesita con un chipset mejorado y mucho espacio para guardar archivos: hasta 128 GB de almacenamiento; una batería de larga duración le permite estar desenchufado durante horas para mantener la diversión en familia Carga rápida, energía durante horas: disfrute durante horas con una sola carga* y vuelva al 100% con el puerto USB C de carga rápida.",
+    price: "8270",
+    uri: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/61krikJxTmL._AC_SL1200_.jpg",
+  },
+  {
+    id: 9,
+    name: "Stanley Classic The Trigger-Action Taza de viaje de piedra caliza de 16 oz.",
+    description:
+      "RESISTENTE Y DURADERA: a diferencia de las tazas normales, está fabricada con Acero inoxidable 18/8, lo que significa que puede soportar golpes Todo está hecho con material libre de BPA para que su bebida se mantenga segura al consumir TAPA.",
+    price: "2170",
+    uri: "https://m.media-amazon.com/images/I/51ThQ67twZL._AC_SL1500_.jpg",
+  },
+  {
+    id: 10,
+    name: "Kyerivs Women's Summer Casual Loose Long Dress Beach Cover Up Cami Maxi Dresses with Pocket.",
+    description: "Kyerivs Women's Summer Casual Loose Long Dress Beach Cover Up Cami Maxi Dresses with Pocket. Adjustable Strap Summer Dresses for Women, Multi colors: black dress, white floral dresses, blue, green red burgundy multi flower dresses for choice. Spaghetti strap dress is a stretchy fabric, sexy v neck not too low, two side handy pockets, loose fitting style, with a split on the sides. Off shoulder backless dress, ankle-length flowy dress design goes well with summer. Occasions for Womens Long Dresses ¡¿Sexy beach dress, party dress for women vacation dress, comfortable daytime sundress, dress maternity photography, dating dresses, go shopping with friends. As a wedding guest dress, pregnancy dress, An flattering flowy dress for graduation/dinner/work is a perfect fit. This basic casual dress for women also perfect for easy-going everyday wear.",
+    price: "2670",
+    uri: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51PDpUw0EcL._AC_UL1500_.jpg",
+  },
+  {
+    id: 11,
+    name: "Apple AirPods (segunda generación)",
+    description: "Acceso rápido a Siri diciendo «Oye Siri». Más de 24 horas de tiempo de escucha total con el estuche de carga. Configuración sin esfuerzo, detección en el oído y cambio automático para una experiencia mágica. Comparte audio fácilmente entre dos juegos de AirPods en tu iPhone, iPad, iPod touch o Apple TV.",
+    price: "5100",
+    uri: "https://m.media-amazon.com/images/I/7120GgUKj3L._AC_SL1500_.jpg",
+  }
+];
