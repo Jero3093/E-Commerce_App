26997772184b23c2d6f68ab65d26d6fd8bb0a8fb
Jeronimo <jeronimotornini@gmail.com>
2022-12-26 17:01:52 -0300

    Adicion de Categorias a los Productos

--- src/Product.js
+++ src/Product.js
@@ -2,6 +2,7 @@ export const Products = [
   {
     id: 1,
     name: "GEARONIC TM Mochila de lona vintage de 21 L para hombres Mochila de cuero Mochila de 15 pulgadas para portátil Mochila de hombro del ejército militar Mochila de hombro Mochila de senderismo Bolsa de café.",
+    category: "Backpacks",
     description:
       "Esta mochila de lona grande tiene un tamaño aproximado de 16 x 18 x 9 pulgadas de profundidad. Lo suficientemente grande como para adaptarse a los elementos esenciales del día a día, como laptop, cuaderno, libro, iPad, almuerzo, cámara, equipo de afeitado, prendas, ropa u otros artículos y accesorios personales esenciales que necesite. MATERIALES DE ALTA CALIDAD: nuestra bolsa de campo Gearonic TM está hecha con tela de lona duradera de alta densidad de alta calidad y cuero PU, esta resistente bolsa cuenta con un diseño clásico pero elegante con solapa de cierre de botón, detalles de hardware de latón vintage, correa para el hombro ajustable y cómoda y múltiples bolsillos para mantenga sus artículos esenciales diarios organizados.",
     price: "1032",
@@ -10,6 +11,7 @@ export const Products = [
   {
     id: 2,
     name: "Chaqueta de béisbol Moshtashio para hombre, estilo universitario, estilo vintage, informal, unisex, ropa de calle, con parche.",
+    category: "Men's Cloth",
     description:
       "Nuestra chaqueta universitaria está hecha de tela ligera y suave. Ligero y elegante, el mejor abrigo activo para la temporada de primavera, otoño e invierno para uso en interiores y exteriores. Chaqueta de corte completo con parche con la letra «B» en el pecho y bordado con patrón de huesos. Cuerpo de tela de alta calidad, mangas de piel sintética con puños elásticos. Bolsillos frontales inclinados. Cierre con botón a presión frontal.",
     price: "3532",
@@ -18,6 +20,7 @@ export const Products = [
   {
     id: 3,
     name: "Moshtashio Chaqueta universitaria universitaria para hombre, chaqueta bomber de béisbol, sudadera vintage, informal, unisex, ropa de calle, abrigos con parche.",
+    category: "Men's Cloth",
     description:
       "Cierre a presión de poliéster importado【TELA PREMIUM】Nuestra chaqueta universitaria está hecha de tela ligera y suave. Ligera y elegante, la mejor chaqueta activa para la temporada de primavera, otoño e invierno para uso en interiores y exteriores.【CARACTERÍSTICAS】Chaqueta de corte completo con un parche con la letra «B» en el pecho y bordado con patrón de huesos. Cuerpo de tela de alta calidad, mangas de piel sintética con puños elásticos. Bolsillos delanteros inclinados. Cierre frontal con botón a presión. Bajo acanalado.",
     price: "5232",
@@ -26,6 +29,7 @@ export const Products = [
   {
     id: 4,
     name: "Beaully Women's Flannel Plaid Jacket Long Sleeve Button Down Chest Pocketed Shirts Coats Shacket.",
+    category: "Women's Cloth",
     description:
       "[Material]-The button up shirt made with high quality flannel. The flannel fabric is soft, wear-resistant, and has good warmth retention.soft to the touch and comfortable to wear.best for autumn and winter. [Feature]-The flannel jacket,Button front top,plaid print,Scoop hemline,Two flap chest pocket,Single button on cuff,Soft fabrication,Oversized fit,Drop shoulder,Unlined.",
     price: "2402",
@@ -34,6 +38,7 @@ export const Products = [
   {
     id: 5,
     name: "CHRLEISURE Butt Lifting Workout Leggings for Women, Scrunch Butt Gym Seamless Booty Tight.",
+    category: "Women's Cloth",
     description:
       "BUTT LIFTING LEGGING - This sensual push-up legging style will smooth both the legs and the tummy while targeted compression creates dramatic definition under the rear-end, Bum lifting bands design cinch your backside with invisible hold around your butt, Works like a push up bra for your booty, providing a little lift",
     price: "670",
@@ -42,6 +47,7 @@ export const Products = [
   {
     id: 6,
     name: "Tableta Android Samsung Galaxy Tab A8, pantalla LCD de 10,5, almacenamiento de 32 GB, batería de larga duración, contenido para niños, interruptor inteligente, memoria ampliable, gris oscuro.",
+    category: "Tecnology",
     description:
       "Potencia y almacenamiento para todos: obtén la potencia, el almacenamiento y la velocidad que tu familia necesita con un chipset mejorado y mucho espacio para guardar archivos: hasta 128 GB de almacenamiento; una batería de larga duración le permite estar desenchufado durante horas para mantener la diversión en familia Carga rápida, energía durante horas: disfrute durante horas con una sola carga* y vuelva al 100% con el puerto USB C de carga rápida.",
     price: "8270",
@@ -50,6 +56,7 @@ export const Products = [
   {
     id: 7,
     name: "Stanley Classic The Trigger-Action Taza de viaje de piedra caliza de 16 oz.",
+    category: "Things",
     description:
       "RESISTENTE Y DURADERA: a diferencia de las tazas normales, está fabricada con Acero inoxidable 18/8, lo que significa que puede soportar golpes Todo está hecho con material libre de BPA para que su bebida se mantenga segura al consumir TAPA.",
     price: "2170",
@@ -58,6 +65,7 @@ export const Products = [
   {
     id: 8,
     name: "Kyerivs Women's Summer Casual Loose Long Dress Beach Cover Up Cami Maxi Dresses with Pocket.",
+    category: "Women's Cloth",
     description:
       "Kyerivs Women's Summer Casual Loose Long Dress Beach Cover Up Cami Maxi Dresses with Pocket. Adjustable Strap Summer Dresses for Women, Multi colors: black dress, white floral dresses, blue, green red burgundy multi flower dresses for choice. Spaghetti strap dress is a stretchy fabric, sexy v neck not too low, two side handy pockets, loose fitting style, with a split on the sides. Off shoulder backless dress, ankle-length flowy dress design goes well with summer. Occasions for Womens Long Dresses ¡¿Sexy beach dress, party dress for women vacation dress, comfortable daytime sundress, dress maternity photography, dating dresses, go shopping with friends. As a wedding guest dress, pregnancy dress, An flattering flowy dress for graduation/dinner/work is a perfect fit. This basic casual dress for women also perfect for easy-going everyday wear.",
     price: "2670",
@@ -66,6 +74,7 @@ export const Products = [
   {
     id: 9,
     name: "Apple AirPods (segunda generación)",
+    category: "Tecnology",
     description:
       "Acceso rápido a Siri diciendo «Oye Siri». Más de 24 horas de tiempo de escucha total con el estuche de carga. Configuración sin esfuerzo, detección en el oído y cambio automático para una experiencia mágica. Comparte audio fácilmente entre dos juegos de AirPods en tu iPhone, iPad, iPod touch o Apple TV.",
     price: "5100",
@@ -74,6 +83,7 @@ export const Products = [
   {
     id: 10,
     name: "FIFA 23 - PlayStation 4",
+    category: "Videogames",
     description:
       "EA SPORTS FIFA 23 trae aún más de la acción y el realismo del fútbol al campo en The World's Game. Juega a los torneos más importantes del fútbol con la Copa Mundial de la FIFA masculina y femenina. Crea la plantilla de tus sueños en FIFA Ultimate Team con FUT Moments y un sistema de química renovado. Experimenta el El Juego Mundial, con más de 19 000 jugadores, más de 700 equipos, más de 100 estadios y más de 30 ligas, Ted Lasso, el entrenador Beard y las estrellas más importantes de la plantilla de la AFC Richmond se integrarán de forma auténtica en FIFA 23.",
     price: "2130",
@@ -82,6 +92,7 @@ export const Products = [
   {
     id: 11,
     name: "Kit combinado de taladro inalámbrico DEWALT ATOMIC 20V MAX*, compacto (DCK278C2)",
+    category: "Tools",
     description:
       "Asegúrese de que se ajusta introduciendo su número de modelo. El DCD708 del kit combinado de taladro inalámbrico es compacto, solo mide 6.3 pulgadas de adelante hacia atrás y está diseñado para caber en espacios reducidos. El DCD708 del kit combinado de herramientas inalámbricas es liviano, solo pesa 2.4 lb. DCD708: la luz LED del pie en el kit combinado de taladro/atornillador de impacto está hecha para una visibilidad superior. DCD708 - Variable El disparador de velocidad permite el control y la precisión en superficies de trabajo delicadas. Motor sin escobillas incorporado DEWALT para un rendimiento y tiempo de ejecución eficientes. El mandril de trinquete de 1/2 pulgada DCD708 proporciona una fuerza superior de agarre de la broca. DCD708 - 340 UWO de potencia para ayudar a hacer el trabajo.",
     price: "12400",
@@ -90,6 +101,7 @@ export const Products = [
   {
     id: 12,
     name: "Camiseta Tommy Hilfiger de manga corta con cuello redondo y bolsillo para hombre",
+    category: "Men's Cloth",
     description:
       "100% algodón, importado, cierre extraíble. Lavar a máquina Diseñada en algodón puro y lavada para mayor suavidad, la camiseta esencial de Tommy Hilfiger para hombre es una de las favoritas de los clientes por una buena razón. PURA FRESCURA: Confeccionada para ser naturalmente transpirable en tela de algodón 100% de primera calidad, esta camiseta para hombre más vendida se mantiene fresca una tras otra. CONSTRUCCIÓN CLÁSICA: Esta camiseta limpia y clásica para hombre presenta todas las firmas de diseño de Tommy Hilfiger, desde el cuello redondo hasta el bolsillo frontal con la bandera bordada en el pecho.",
     price: "340",
