1bc4f204dc9d55d4e34c1a9405369cd8084e1ef6
Jeronimo <jeronimotornini@gmail.com>
2023-01-04 00:23:15 -0300

    Funciones para total precio y eliminar productos

--- Screens/Cart.js
+++ Screens/Cart.js
@@ -17,7 +17,7 @@ import { Ionicons } from "@expo/vector-icons"; //Expo Icons
 export default function CartScreen({ navigation }) {
   const [product, setproduct] = useState(); //Product State
 
-  const [total, settotal] = useState(null); // Total Price State
+  const [Total, setTotal] = useState(null); // Total Price State
 
   useEffect(() => {
     const unsubscribe = navigation.addListener("focus", () => {
@@ -39,18 +39,46 @@ export default function CartScreen({ navigation }) {
         }
       });
       setproduct(ProductData);
+      getTotal(ProductData);
     } else {
       setproduct(false);
+      getTotal(false);
     }
   }; // Get the data stoterd whith Async Storage
 
+  const getTotal = (ProductData) => {
+    let total = 0;
+    for (let index = 0; index < ProductData.length; index++) {
+      let ProductPrice = ProductData[index].ProductPrice;
+      total = total + ProductPrice;
+    }
+
+    setTotal(total);
+  }; //Get the total price to pay
+
+  const removeProduct = async (id) => {
+    let ItemArray = await AsyncStorage.getItem("cartItem");
+    ItemArray = JSON.parse(ItemArray);
+    if (ItemArray) {
+      let array = ItemArray;
+      for (let index = 0; index < array.length; index++) {
+        if (array[index] == id) {
+          array.splice(index, 1);
+        }
+
+        await AsyncStorage.setItem("cartItem", JSON.stringify(array));
+        getDataFromJson();
+      }
+    }
+  }; //Remove the Products from the Cart
+
   const CartAlert = () => {
     Alert.alert("Check", "Payment was Successful");
-  }; // Pay Button Alert
+  }; //Pay Button Alert
 
   const CartProduct = (data, index) => {
     return (
-      <View style={styles.ProductsContainer}>
+      <View style={styles.ProductsContainer} key={data.id}>
         <Image
           source={data.Image}
           style={{
@@ -61,17 +89,17 @@ export default function CartScreen({ navigation }) {
           }}
         />
         <View style={styles.ProductContent}>
-          <Text numberOfLines={2} style={{ fontSize: 16, marginBottom: 5 }}>
+          <Text numberOfLines={1} style={{ fontSize: 16, marginBottom: 5 }}>
             {data.name}
           </Text>
           <Text style={{ fontSize: 15, marginBottom: 5 }}>${data.price}</Text>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={() => removeProduct(data.id)}>
             <EvilIcons name="trash" size={30} color="black" />
           </TouchableOpacity>
         </View>
       </View>
     );
-  }; // Cart Product Container
+  }; //Cart Product Container
 
   return (
     <SafeAreaView style={styles.Container}>
@@ -80,7 +108,10 @@ export default function CartScreen({ navigation }) {
           <Ionicons name="md-chevron-back-outline" size={33} color="black" />
         </TouchableOpacity>
         <Text style={styles.TopBarTitle}>Cart</Text>
-        <Text style={styles.TopBarTotal}>$1445</Text>
+        <View style={{ flexDirection: "column", alignItems: "center" }}>
+          <Text style={styles.TopBarTotal}>Total:</Text>
+          <Text style={styles.TopBarTotal}>${Total}</Text>
+        </View>
       </View>
       <ScrollView>
         <View>{product ? product.map(CartProduct) : null}</View>
@@ -93,7 +124,7 @@ export default function CartScreen({ navigation }) {
       </View>
     </SafeAreaView>
   );
-}
+}//Screen Container
 
 const styles = StyleSheet.create({
   Container: {
@@ -121,6 +152,8 @@ const styles = StyleSheet.create({
     padding: 15,
     marginVertical: 5,
     borderRadius: 10,
+    marginHorizontal: 10,
+    overflow: "hidden",
   },
   ProductContent: {
     flexDirection: "column",
