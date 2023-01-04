debf8f53493ed6bfefeef4a1b295663412f8f95c
Jeronimo <jeronimotornini@gmail.com>
2023-01-03 23:33:58 -0300

    Funcion de mostrar el producto guardado

--- Screens/Cart.js
+++ Screens/Cart.js
@@ -4,23 +4,54 @@ import {
   StyleSheet,
   Text,
   Image,
-  FlatList,
   TouchableOpacity,
   Alert,
 } from "react-native";
+import React, { useState, useEffect } from "react";
 import { Products } from "../src/Product"; //Products JSON
-import { EvilIcons } from "@expo/vector-icons";//Expo Icons
+import { EvilIcons } from "@expo/vector-icons"; //Expo Icons
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { FlashList } from "@shopify/flash-list"; // Flashlist Component
 
 export default function CartScreen({ navigation }) {
+  const [product, setproduct] = useState(); //Product State
+
+  const [total, settotal] = useState(null); // Total Price State
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      getDataFromJson();
+    });
+
+    return unsubscribe;
+  }, [navigation]); //Get the data from the JSON once
+
+  const getDataFromJson = async () => {
+    let Items = await AsyncStorage.getItem("cartItem");
+    Items = JSON.parse(Items);
+    let ProductData = [];
+    if (Items) {
+      Products.forEach((data) => {
+        if (Items.includes(data.id)) {
+          ProductData.push(data);
+          return;
+        }
+      });
+      setproduct(ProductData);
+    } else {
+      setproduct(false);
+    }
+  }; // Ger the data stoterd whith Async Storage
+
   const CartAlert = () => {
     Alert.alert("Check", "Payment was Successful");
-  };
+  }; // Pay Button Alert
 
-  const CartProduct = ({ Products }) => {
+  const CartProduct = (data, index) => {
     return (
       <View style={styles.ProductsContainer}>
         <Image
-          source={Products.Image}
+          source={data.Image}
           style={{
             width: 60,
             height: 60,
@@ -30,26 +61,29 @@ export default function CartScreen({ navigation }) {
         />
         <View style={styles.ProductContent}>
           <Text numberOfLines={2} style={{ fontSize: 16, marginBottom: 5 }}>
-            {Products?.name}
+            {data.name}
           </Text>
-          <Text style={{ fontSize: 15, marginBottom: 5 }}>
-            ${Products?.price}
-          </Text>
+          <Text style={{ fontSize: 15, marginBottom: 5 }}>${data.price}</Text>
           <TouchableOpacity>
             <EvilIcons name="trash" size={30} color="black" />
           </TouchableOpacity>
         </View>
       </View>
     );
-  };
+  }; // Cart Product Container
 
   return (
-    <SafeAreaView style={styles.CartContainer}>
-      <FlatList
-        data={Products}
-        renderItem={({ item }) => <CartProduct Products={item} />}
-        showsVerticalScrollIndicator={false}
-      />
+    <SafeAreaView style={styles.Container}>
+      <View style={styles.TopBar}>
+
+      </View>
+      <Text>
+        {product
+          ? product.map((data, index) => {
+              return <Text>{data.name} </Text>;
+            })
+          : null}
+      </Text>
       <View style={styles.BuyBottonContainer}>
         <TouchableOpacity style={styles.BuyBotton} onPress={CartAlert}>
           <Text style={styles.BottonText}>Finish Payment</Text>
@@ -60,7 +94,7 @@ export default function CartScreen({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-  CartContainer: {
+  Container: {
     flex: 1,
     backgroundColor: "#12121212",
   },
@@ -85,8 +119,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#121212",
     padding: 10,
     borderRadius: 50,
-    position: "absolute",
-    bottom: 20,
   },
   BottonText: {
     color: "#fff",
