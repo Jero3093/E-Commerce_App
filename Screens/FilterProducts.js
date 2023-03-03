b98565b22253ea08bbc1daa329587794dd383e36
Jeronimo <jeronimotornini@gmail.com>
2023-03-03 17:49:18 -0300

    Separacion de Componentes

--- Screens/FilterProducts.js
+++ Screens/FilterProducts.js
@@ -1,19 +1,11 @@
-import {
-  Text,
-  View,
-  StyleSheet,
-  FlatList,
-  TouchableOpacity,
-  Image,
-} from "react-native";
 import React, { useState, useEffect } from "react";
-import { FlashList } from "@shopify/flash-list";
-import { Products } from "../src/Product";
+import { View, StyleSheet, FlatList } from "react-native";
+import { Products } from "../src/Product"; //Products Database
+import { FlashList } from "@shopify/flash-list"; //Flash List Component
+import { FilteredProductCard } from "../src/Components/FilterScreen/FilteredProductCard"; //Filtered Product Card Component
 
 export default function FilterProducts({ route, navigation }) {
-  const { ProductCategory } = route.params; //Serach the Category of the product from the JSON
-
-  const { ProductId } = route.params; //Serach the ID of the product from the
+  const { CategoryName } = route.params; //Serach the Category of the product from the JSON
 
   const [product, setproduct] = useState({}); //Product State
 
@@ -25,55 +17,17 @@ export default function FilterProducts({ route, navigation }) {
     return unsubscribe;
   }, [navigation]);
 
-  const getDataFromJson = async () => {
+  const getDataFromJson = () => {
     for (let index = 0; index < Products.length; index++) {
-      if (
-        Products[index].category === ProductCategory &&
-        Products[index].id === ProductId
-      ) {
+      if (Products[index].category === CategoryName) {
         setproduct(Products[index]);
-        return;
       }
     }
   }; //Search for an specific ID and Category then update the product states
 
-  const ProductFilterCard = () => {
-    return (
-      <View style={styles.CardContainer}>
-        <TouchableOpacity
-          onPress={() =>
-            navigation.navigate("Product Details", { ProductId: Products.id })
-          }
-        >
-          <View style={styles.ImageContainer}>
-            <Image
-              source={product.Image}
-              style={{
-                width: 300,
-                height: 220,
-                resizeMode: "contain",
-                marginBottom: 30,
-              }}
-            />
-          </View>
-          <Text style={styles.CardCategory}>{product.category} </Text>
-          <Text numberOfLines={2} style={styles.CardName}>
-            {product?.name}
-          </Text>
-          <Text style={styles.CardPrice}>${product.price}</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  };
-
   return (
     <View style={styles.Container}>
-      <FlashList
-        data={Products}
-        keyExtractor={Products.category}
-        renderItem={({ item }) => <ProductFilterCard Products={item} />}
-        estimatedItemSize={50}
-      />
+      <FilteredProductCard Data={product} />
     </View>
   );
 }
@@ -89,6 +43,8 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     padding: 15,
     marginBottom: 30,
+    marginTop: 20,
+    marginHorizontal: 10,
   },
   ImageContainer: {
     alignItems: "center",
