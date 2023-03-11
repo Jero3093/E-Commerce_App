fd8debaf213303f14f75bc347b5e0e7fec9689ee
Jeronimo <jeronimotornini@gmail.com>
2023-03-11 16:40:42 -0300

    Agregada a la Store estados de Cart

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { ProductSlice } from "./ProductSlice"; //Product Slice Component
+import { CartSlice } from "./CartSlice"; //Cart Items Slice Component
 
 export const store = configureStore({
   reducer: {
     products: ProductSlice.reducer, //Products Data
+    CartItems: CartSlice.reducer, //Cart Items Data
   },
 }); //Function to Store all the states in the initial state to use it in the entire app
